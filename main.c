8232da3b079f5ab37c84faff7d34307fae2d5ba4
ysb4642 <ysb4642@naver.com>
2022-05-14 19:25:24 +0900

    hello

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "include/ft_printf.h"
+#include "./include/ft_printf.h"
 #include <stdio.h>
 
 int main(void) 
@@ -10,13 +10,31 @@ int main(void)
 	ft_printf("%d\n", 10);
 	printf("%d\n", 10);
 
+	ft_printf("%+d\n", -10);
+	printf("%+d\n", -10);
+
+	ft_printf("% d\n", 10);
+	printf("% d\n", 10);
+
+	ft_printf("%#x\n", 10);
+	printf("%#x\n", 10);
+
 	ft_printf("%s\n", "hello world");
 	printf("%s\n", "hello world");
+
+	ft_printf("%s\n", "안녕하세요");
+	printf("%s\n", "안녕하세요");
 
 	ft_printf("%s\n", s1);
 	printf("%s\n", s1);
 
 	ft_printf("%p\n", s2);
 	printf("%p\n", s2);
+
+	ft_printf("%%\n");
+	printf("%%\n");
+
+	ft_printf("%c\n", 'a');
+	printf("%c\n", 'a');
 
 }
